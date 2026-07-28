44d20767ec9651e3a868c6317d7edfd36c88ea7f
嘻哈哈 <100525935+Powerpowername@users.noreply.github.com>
2026-07-29 01:29:31 +0800

    Create global.h

---
+++ include/global.h
@@ -0,0 +1,25 @@
+#pragma once
+#define NOMINMAX
+#include <windows.h>
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_dx12.h"
+#include "imgui/backends/imgui_impl_win32.h"
+#include <wrl.h>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <DirectXMath.h>
+#include "Open3D/Open3D.h"
+#include "directx/d3d12.h"
+// #include <stdint.h>
+#include <print>
+#include <mutex>
+#include "directx/d3dx12.h"
+#include <d3dcompiler.h>
+#include <functional>
+#include <dxgi1_4.h>
+#include <array>
+
+
+#define DXENGINE_DEFAULT 0
