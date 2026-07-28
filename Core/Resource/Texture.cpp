30ec77ecd16ddc2f1e2cc415e3c0b3c1b3b44f37
嘻哈哈 <100525935+Powerpowername@users.noreply.github.com>
2026-07-29 01:22:06 +0800

    Create Texture.cpp with initial setup
    Added Texture.cpp with initial includes and commented-out function.

---
+++ Core/Resource/Texture.cpp
@@ -0,0 +1,11 @@
+#include "Texture.hpp"
+
+
+// void TextureLODInit(){
+
+//     textureLODRootSignature.Reset();
+//     textureLODRootSignature.AddRootConstants(4, 0);
+//     textureLODRootSignature.AddUAVTable(1, 0);
+//     textureLODRootSignature.AddUAVTable(1, 1);
+//     textureLODRootSignature.CreateRootSignature(device, D3D12_ROOT_SIGNATURE_FLAG_NONE);
+// }
